e067cf21d7494f01197efe4dd10c1bda57182ea5
Dong <ddliuhb@gmail.com>
2012-05-06 09:04:17 +0800

    implement unittest

--- test.py
+++ test.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python
+import unittest
 import util, svn
-s = svn.svn('/home/dong/projects/sae/apps/vdisksina/2/code')
-print(s.info())
+
+class TestSVN(unittest.TestCase):
+    def setUp(self):
+        self.svn = svn.svn('/home/dong/projects/sae/apps/ddliu')
+
+    def test_info(self):
+        info = self.svn.info()
+        self.assertRegexpMatches(info['revision'], '^\d+$', 'revision is numeric')
+
+    def test_update(self):
+        pass
+
+    def test_commit(self):
+        pass
+
+class TestSync(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_sync(self):
+        print('aa')
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
